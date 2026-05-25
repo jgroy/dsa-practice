dac8d6a9cb327f421b7cfe65cdf8c45986ad4a45
jgroy <jgroy8905@gmail.com>
2026-05-25 15:29:56 +0530

    LeetCode #136 problem Single Number ,solved using HashSet alternate is XOR

---
+++ src/LeetCode136SingleNumber.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i =0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                set.remove(nums[i]);
+            }
+            else
+            set.add(nums[i]);
+        }
+        for(int i = 0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+}
